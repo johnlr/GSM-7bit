4a3b7a2f4a01efca8bf20a4b46cacea9793fce62
John La Rooy <johnl@smartguide.com.au>
2016-01-11 16:52:41 +1100

    Initial commit
    	new file:   gsm7bit_python2.py
    	new file:   gsm7bit_python3.py

---
+++ gsm7bit_python2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*- 
+gsm = (u"@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>"
+       u"?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ`¿abcdefghijklmnopqrstuvwxyzäöñüà")
+ext = (u"````````````````````^```````````````````{}`````\\````````````[~]`"
+       u"|````````````````````````````````````€``````````````````````````")
+
+def gsm_encode(plaintext):
+    res = ""
+    for c in plaintext:
+        idx = gsm.find(c)
+        if idx != -1:
+            res += chr(idx)
+            continue
+        idx = ext.find(c)
+        if idx != -1:
+            res += chr(27) + chr(idx)
+    return res.encode('hex')
+
+print gsm_encode(u"Hello World")
---
+++ gsm7bit_python3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*- 
+import binascii
+gsm = ("@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?"
+       "¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ`¿abcdefghijklmnopqrstuvwxyzäöñüà")
+ext = ("````````````````````^```````````````````{}`````\\````````````[~]`"
+       "|````````````````````````````````````€``````````````````````````")
+
+def gsm_encode(plaintext):
+    res = ""
+    for c in plaintext:
+        idx = gsm.find(c);
+        if idx != -1:
+            res += chr(idx)
+            continue
+        idx = ext.find(c)
+        if idx != -1:
+            res += chr(27) + chr(idx)
+    return binascii.b2a_hex(res.encode('utf-8'))
+
+print(gsm_encode("Hello World"))
